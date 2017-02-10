faecc177ea04db4b7946f82779696a65a7d41536
Jordan Hayashi <jhhayashi@gmail.com>
2017-02-10 16:32:38 -0500

    Initial commit with package.json and basic server

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const config = require('./app/models/config');
+const routes = require('./routes/index');
+
+let app = express();
+app.locals.config = config;
+if (app.get('env') === 'development') app.locals,dev = true;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'app', 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after adding public/favicon.ico
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// log requests
+if (app.locals.dev) app.use(logger('dev'));
+
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// development error handler
+if (app.locals.dev) {
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500);
+        console.log(err);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+const server = app.listen(config.port);
+console.log('Listening at http://localhost:%s in %s mode',
+    server.address().port, app.get('env'));
+
+module.exports = app;
